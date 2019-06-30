c9bd5e64965a38cd055097f0c954856958617424
Liang-yc <1697205782@qq.com>
2019-06-30 10:02:35 +0800

    Update ssq_data.py

--- ssq_data.py
+++ ssq_data.py
@@ -10,6 +10,101 @@ import cv2
 from PIL import Image
 import os
 
+def get_exl_data_by_period(exl_path='./ssq.xls',random_order=False,use_resnet=False,times=10):
+    ssq_data = []
+    xls_data = get_data(exl_path)
+    # print(type(xls_data))
+    if use_resnet:
+        if random_order:
+            for i in range(2, len(xls_data['data'])-times):
+                # print(type(xls_data['data'][i][2]))
+                # aaaa=xls_data['data'][i][2]
+                if xls_data['data'][i] == [] or xls_data['data'][i+times] == []:
+                    break
+                temp=[]
+                for j in range(times):
+                    temp.append([
+                    [xls_data['data'][i+j][9] - 1],
+                    [xls_data['data'][i+j][10] - 1],
+                    [xls_data['data'][i+j][11] - 1],
+                    [xls_data['data'][i+j][12] - 1],
+                    [xls_data['data'][i+j][13] - 1],
+                    [xls_data['data'][i+j][14] - 1],
+                    [xls_data['data'][i+j][8] + 32],
+                    [xls_data['data'][i+j][15] / 10000000000],
+                    [xls_data['data'][i+j][16] / 10000000000],
+                    [xls_data['data'][i+j][17] / 10000000],
+                    [xls_data['data'][i+j][19] / 10000000],
+                    [xls_data['data'][i+j][21] / 10000000],
+                    [xls_data['data'][i+j][23] / 10000000],
+                    [xls_data['data'][i+j][25] / 10000000],
+                    [xls_data['data'][i+j][27] / 10000000]
+                ])
+
+                # print(temp.shape)
+                #
+                ssq_data.append(np.asarray(temp))
+        else:
+            for i in range(2, len(xls_data['data'])-times):
+                # print(type(xls_data['data'][i][2]))
+                # aaaa=xls_data['data'][i][2]
+                if xls_data['data'][i] == [] or xls_data['data'][i+times] == []:
+                    break
+                temp=[]
+
+                for j in range(times):
+
+                    temp.append([
+                    [xls_data['data'][i+j][2] - 1],
+                    [xls_data['data'][i+j][3] - 1],
+                    [xls_data['data'][i+j][3] - 1],
+                    [xls_data['data'][i+j][5] - 1],
+                    [xls_data['data'][i+j][6] - 1],
+                    [xls_data['data'][i+j][7] - 1],
+                    [xls_data['data'][i+j][8] + 32],
+                    [xls_data['data'][i+j][15] / 10000000000],
+                    [xls_data['data'][i+j][16] / 10000000000],
+                    [xls_data['data'][i+j][17] / 10000000],
+                    [xls_data['data'][i+j][19] / 10000000],
+                    [xls_data['data'][i+j][21] / 10000000],
+                    [xls_data['data'][i+j][23] / 10000000],
+                    [xls_data['data'][i+j][25] / 10000000],
+                    [xls_data['data'][i+j][27] / 10000000]
+                ])
+                ssq_data.append(np.asarray(temp))
+        return ssq_data
+
+    if random_order:
+        for i in range(2, len(xls_data['data'])):
+            # print(type(xls_data['data'][i][2]))
+            # aaaa=xls_data['data'][i][2]
+            if xls_data['data'][i] == []:
+                break
+            temp = np.asarray([
+                xls_data['data'][i][9] - 1,
+                xls_data['data'][i][10] - 1,
+                xls_data['data'][i][11] - 1,
+                xls_data['data'][i][12] - 1,
+                xls_data['data'][i][13] - 1,
+                xls_data['data'][i][14] - 1,
+                xls_data['data'][i][8] + 32])
+            ssq_data.append(temp)
+    else:
+        for i in range(2, len(xls_data['data'])):
+            # print(type(xls_data['data'][i][2]))
+            # aaaa=xls_data['data'][i][2]
+            if xls_data['data'][i] == []:
+                break
+            temp = np.asarray([
+                xls_data['data'][i][2] - 1,
+                xls_data['data'][i][3] - 1,
+                xls_data['data'][i][4] - 1,
+                xls_data['data'][i][5] - 1,
+                xls_data['data'][i][6] - 1,
+                xls_data['data'][i][7] - 1,
+                xls_data['data'][i][8] + 32])
+            ssq_data.append(temp)
+    return ssq_data
 
 def get_red(exl_path='./ssq.xls', random_order=False):
     ssq_data=[]
